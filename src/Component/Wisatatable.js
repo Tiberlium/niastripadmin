e819e3244d9cc3554169500d40ba9af26e332c18
Tiberlium <lonew047@gmail.com>
2022-03-31 20:01:12 +0700

    perbaikan

--- src/Component/Wisatatable.js
+++ src/Component/Wisatatable.js
@@ -10,7 +10,12 @@ import {
   Button,
 } from "@chakra-ui/react";
 import { db } from "../Firebase";
-import { BsFillPencilFill, BsFillTrashFill } from "react-icons/bs";
+import {
+  BsFillPencilFill,
+  BsFillTrashFill,
+  BsFillPlusCircleFill,
+} from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 export default class Wisatatable extends Component {
   state = {
@@ -33,7 +38,7 @@ export default class Wisatatable extends Component {
   }
 
   async remove(id) {
-    const docRef = await db.collection("Wisata").doc(id);
+    const docRef = db.collection("Wisata").doc(id);
     docRef.delete();
     this.get();
   }
@@ -41,59 +46,65 @@ export default class Wisatatable extends Component {
   componentDidMount() {
     this.get();
   }
+
   render() {
     return (
-      <Table variant={"striped"} colorScheme="telegram" size="sm">
-        <TableCaption fontWeight={"bold"} fontSize={"20"}>
-          Wisata List
-        </TableCaption>
-        <Thead>
-          <Tr>
-            <Th>No</Th>
-            <Th>Nama</Th>
-            <Th>Kabupaten</Th>
-            <Th>Kota</Th>
-            <Th>Latitude/Longitude</Th>
-            <Th>Aksi Edit</Th>
-            <Th>Aksi Hapus</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {this.state.data.map((doc, index) => (
-            <Tr key={doc.id}>
-              <Td>{index}</Td>
-              <Td>{doc.data.Nama}</Td>
-              <Td>{doc.data.Kabupaten}</Td>
-              <Td>{doc.data.Kecamatan}</Td>
-              <Td>
-                {doc.data.Latitude}/{doc.data.Longitude}
-              </Td>
-              <Td>
-                <Button
-                  colorScheme={"blue"}
-                  variant="solid"
-                  size="sm"
-                  leftIcon={<BsFillPencilFill />}
-                  onClick={() => this.props.edit(doc.id)}
-                >
-                  Edit
-                </Button>
-              </Td>
-              <Td>
-                <Button
-                  colorScheme={"red"}
-                  variant="solid"
-                  size="sm"
-                  leftIcon={<BsFillTrashFill />}
-                  onClick={() => this.remove(doc.id)}
-                >
-                  Delete
-                </Button>
-              </Td>
+      <>
+        <Button colorScheme="blue" mb={5} leftIcon={<BsFillPlusCircleFill />}>
+          Tambahkan yang baru
+        </Button>
+        <Table variant={"striped"} colorScheme="telegram" size="sm">
+          <TableCaption fontWeight={"bold"} fontSize={"20"}>
+            Wisata List
+          </TableCaption>
+          <Thead>
+            <Tr>
+              <Th>No</Th>
+              <Th>Nama</Th>
+              <Th>Kabupaten</Th>
+              <Th>Kota</Th>
+              <Th>Latitude/Longitude</Th>
+              <Th>Aksi Edit</Th>
+              <Th>Aksi Hapus</Th>
             </Tr>
-          ))}
-        </Tbody>
-      </Table>
+          </Thead>
+          <Tbody>
+            {this.state.data.map((doc, index) => (
+              <Tr key={doc.id}>
+                <Td>{index}</Td>
+                <Td>{doc.data.Nama}</Td>
+                <Td>{doc.data.Kabupaten}</Td>
+                <Td>{doc.data.Kecamatan}</Td>
+                <Td>
+                  {doc.data.Latitude}/{doc.data.Longitude}
+                </Td>
+                <Td>
+                  <Button
+                    colorScheme={"blue"}
+                    variant="solid"
+                    size="sm"
+                    leftIcon={<BsFillPencilFill />}
+                    onClick={() => this.props.edit(doc.id)}
+                  >
+                    Edit
+                  </Button>
+                </Td>
+                <Td>
+                  <Button
+                    colorScheme={"red"}
+                    variant="solid"
+                    size="sm"
+                    leftIcon={<BsFillTrashFill />}
+                    onClick={() => this.remove(doc.id)}
+                  >
+                    Delete
+                  </Button>
+                </Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </>
     );
   }
 }
