50782a8224ea588276f005d81918dce0723adec9
Tiberlium <lonew047@gmail.com>
2022-04-06 22:04:37 +0700

    perbaikan

--- src/App.js
+++ src/App.js
@@ -11,26 +11,84 @@ import {
   Event,
   User,
   Userdetail,
+  Home,
 } from "./Pages";
 
-import { Box, List, ListItem, Text, Flex, Spacer } from "@chakra-ui/react";
+import {
+  IoHomeSharp,
+  IoBedSharp,
+  IoCalendarNumberSharp,
+  IoFastFood,
+  IoPersonSharp,
+  IoCashSharp,
+  IoBuildSharp,
+  IoNewspaperSharp,
+} from "react-icons/io5";
+import { Flex, List, ListItem, Text, Box } from "@chakra-ui/react";
 
-
-class App extends react.Component {
-  render() {
-    return (
-      <Box>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<User />} />
-            <Route path="/Tambahkandatawisata" element={<Managewisata />} />
-            <Route path="/Editwisata/:id" element={<Managewisata />} />
-            <Route />
-          </Routes>
-        </BrowserRouter>
-      </Box>
-    );
-  }
+function App() {
+  return (
+    <BrowserRouter>
+      <List spacing={2} mr={4} ml={4}>
+        <ListItem backgroundColor={"azure"} p={2} borderRadius={10} w={"52"}>
+          <Link to="/">
+            <Flex>
+              <Box mr={5}>
+                <IoHomeSharp color="blue" size={20} />
+              </Box>
+              <Text fontWeight="semibold">Home</Text>
+            </Flex>
+          </Link>
+        </ListItem>
+        <ListItem backgroundColor={"azure"} p={2} borderRadius={10} w={"52"}>
+          <Link to="/">
+            <Flex>
+              <Box mr={5}>
+                <IoCashSharp color="blue" size={20} />
+              </Box>
+              <Text fontWeight="semibold">Transaction</Text>
+            </Flex>
+          </Link>
+        </ListItem>
+        <ListItem backgroundColor={"azure"} p={2} borderRadius={10} w={"52"}>
+          <Link to="/">
+            <Flex>
+              <Box mr={5}>
+                <IoPersonSharp color="blue" size={20} />
+              </Box>
+              <Text fontWeight="semibold">User</Text>
+            </Flex>
+          </Link>
+        </ListItem>
+        <ListItem backgroundColor={"azure"} p={2} borderRadius={10} w={"52"}>
+          <Link to="/">
+            <Flex>
+              <Box mr={5}>
+                <IoBuildSharp color="blue" size={20} />
+              </Box>
+              <Text fontWeight="semibold">Manage</Text>
+            </Flex>
+          </Link>
+        </ListItem>
+        <ListItem backgroundColor={"azure"} p={2} borderRadius={10} w={"52"}>
+          <Link to="/">
+            <Flex>
+              <Box mr={5}>
+                <IoNewspaperSharp color="blue" size={20} />
+              </Box>
+              <Text fontWeight="semibold">Report</Text>
+            </Flex>
+          </Link>
+        </ListItem>
+      </List>
+      <Routes>
+        <Route path="/" element={<User />} />
+        <Route path="/Tambahkandatawisata" element={<Managewisata />} />
+        <Route path="/Editwisata/:id" element={<Managewisata />} />
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
