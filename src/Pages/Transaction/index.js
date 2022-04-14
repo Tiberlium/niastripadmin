f7438d8a34dbf0ed6ebf623229c6637cb4668aea
Tiberlium <lonew047@gmail.com>
2022-04-14 20:59:54 +0700

    perbaikan

--- src/Pages/Transaction/index.js
+++ src/Pages/Transaction/index.js
@@ -1,5 +1,31 @@
 import React from "react";
+import {
+  Box,
+  Table,
+  Thead,
+  Tbody,
+  Th,
+  Tr,
+  Td,
+  TableCaption,
+} from "@chakra-ui/react";
 
 export default function Transaction() {
-  return <div>Transaction</div>;
+  return (
+    <Table variant={"striped"}>
+      <TableCaption>Transaction</TableCaption>
+      <Thead>
+        <Th>No</Th>
+        <Th>Id order</Th>
+        <Th>Pengguna</Th>
+        <Th>Status</Th>
+        <Th>Jumlah</Th>
+      </Thead>
+      {/* <Tbody>
+        <Tr>
+         
+        </Tr>
+      </Tbody> */}
+    </Table>
+  );
 }
