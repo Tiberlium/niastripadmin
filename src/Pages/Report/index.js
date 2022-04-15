64da24b164f41558a55fc08d12596590c6f2ff22
Tiberlium <lonew047@gmail.com>
2022-04-15 21:14:45 +0700

    perbaikan

--- src/Pages/Report/index.js
+++ src/Pages/Report/index.js
@@ -1,7 +1,69 @@
-import React from 'react'
+import React from "react";
+import {
+  Tabs,
+  TabList,
+  Tab,
+  TabPanel,
+  TabPanels,
+  Box,
+  Thead,
+  Tbody,
+  Th,
+  Td,
+  Table,
+  TableCaption,
+  TableContainer,
+  Text,
+} from "@chakra-ui/react";
 
 export default function Report() {
   return (
-    <div>Report</div>
-  )
+    <Box>
+      <Text fontSize={'5xl'} mb={5}>
+        Report
+      </Text>
+      <Tabs variant={"soft-rounded"} isFitted>
+        <TabList mb="1em">
+          <Tab>Penginapan</Tab>
+          <Tab>Event</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <TableContainer>
+              <Table variant={"striped"}>
+                <TableCaption>Data Report Penginapan</TableCaption>
+                <Thead>
+                  <Th>No</Th>
+                  <Th>Order id</Th>
+                  <Th>Nama</Th>
+                  <Th>Kategori</Th>
+                  <Th>Biaya</Th>
+                  <Th>Komisi (10%)</Th>
+                  <Th>Tanggal Transaksi</Th>
+                  <Th>Detail</Th>
+                </Thead>
+              </Table>
+            </TableContainer>
+          </TabPanel>
+          <TabPanel>
+            <TableContainer>
+              <Table variant={"striped"}>
+                <TableCaption>Data Report Event</TableCaption>
+                <Thead>
+                  <Th>No</Th>
+                  <Th>Order id</Th>
+                  <Th>Nama</Th>
+                  <Th>Kategori</Th>
+                  <Th>Biaya</Th>
+                  <Th>Komisi (10%)</Th>
+                  <Th>Tanggal Transaksi</Th>
+                  <Th>Detail</Th>
+                </Thead>
+              </Table>
+            </TableContainer>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </Box>
+  );
 }
