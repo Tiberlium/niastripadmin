c75f2a27ff2594b93da28731bf500bee9c032a43
Tiberlium <lonew047@gmail.com>
2022-04-24 21:00:35 +0700

    perbaikan

--- src/Pages/User/index.js
+++ src/Pages/User/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import {
   Table,
@@ -14,9 +14,30 @@ import {
   Box,
   TableContainer,
 } from "@chakra-ui/react";
-import { IoMdWarning, IoMdTrash } from "react-icons/io";
+import { IoIosInformationCircle, IoMdTrash } from "react-icons/io";
+
+import { db } from "../../Firebase";
 
 export default function User() {
+  const [data, setdata] = useState([]);
+
+  const get = async () => {
+    let x = [];
+    const docRef = await db.collection("Users").get();
+    docRef.docs.map((doc) => {
+      x.push({
+        id: doc.id,
+        data: doc.data(),
+      });
+    });
+
+    setdata(x);
+  };
+
+  useEffect(() => {
+    get();
+  }, []);
+
   return (
     <Box>
       <Text fontSize={"5xl"} mt={10} ml={5}>
@@ -30,33 +51,39 @@ export default function User() {
               <Th>No</Th>
               <Th>Id</Th>
               <Th>Nama</Th>
-              <Th>Email atau phone</Th>
+              <Th>Gender</Th>
               <Th>Detail</Th>
               <Th>Hapus</Th>
             </Tr>
           </Thead>
           <Tbody>
-            {/* <Tr>
-              <Td>
-                <Button
-                  colorScheme="red"
-                  size="sm"
-                  leftIcon={<IoMdTrash />}
-                  mr={5}
-                  onClick={this.props.hapus}
-                >
-                  Hapus
-                </Button>
-                <Button
-                  colorScheme="yellow"
-                  size="sm"
-                  leftIcon={<IoMdWarning />}
-                  onClick={this.props.ban}
-                >
-                  Larang
-                </Button>
-              </Td>
-            </Tr> */}
+            {data.map((doc, index) => (
+              <Tr>
+                <Td>{index}</Td>
+                <Td>{doc.data.id}</Td>
+                <Td>{doc.data.name}</Td>
+                <Td>{doc.data.gender}</Td>
+                <Td>
+                  <Button
+                    colorScheme="green"
+                    size="sm"
+                    leftIcon={<IoIosInformationCircle />}
+                  >
+                    Detail
+                  </Button>
+                </Td>
+                <Td>
+                  <Button
+                    colorScheme="red"
+                    size="sm"
+                    leftIcon={<IoMdTrash />}
+                    mr={5}
+                  >
+                    Hapus
+                  </Button>
+                </Td>
+              </Tr>
+            ))}
           </Tbody>
         </Table>
       </TableContainer>
