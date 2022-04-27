10ce321fe9e5991e9c5ba6f7c2676801b75aaf3e
Tiberlium <lonew047@gmail.com>
2022-04-27 23:07:32 +0700

    perbaikan

--- src/Pages/Userdetail/index.js
+++ src/Pages/Userdetail/index.js
@@ -1,5 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { Box, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Text,
+  Table,
+  Td,
+  Tr,
+  Th,
+  TableCaption,
+  Thead,
+  Tbody,
+} from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import { db } from "../../Firebase";
 import Detailuser from "../../Component/Detailuser";
@@ -17,11 +27,9 @@ export default function Userdetail() {
     get();
   }, []);
 
-
-  console.log(data.reservation);
-
   return (
     <Box>
+      <Text fontSize="5xl">User Detail</Text>
       <Detailuser
         nama={data.name}
         alamat={data.address}
@@ -31,6 +39,36 @@ export default function Userdetail() {
         email={data.email}
         gambar={data.gambar}
       />
+
+      <Table variant="striped" size="md">
+        <TableCaption>Order History</TableCaption>
+        <Thead>
+          <Th>No</Th>
+          <Th>Nama</Th>
+          <Th>Order id</Th>
+          <Th>Check IN</Th>
+          <Th>Check Out</Th>
+          <Th>Harga</Th>
+          <Th>Jumlah Orang</Th>
+        </Thead>
+        <Tbody>
+          {data["reservation"]?.map((doc, index) => (
+            <Tr>
+              <Td>{(index = 1)}</Td>
+              <Td>{doc.nama}</Td>
+              <Td>
+                <Text isTruncated noOfLines={[1, 2, 3]}>
+                  {doc.orderId}
+                </Text>
+              </Td>
+              <Td>{doc.checkIN}</Td>
+              <Td>{doc.checkOUT}</Td>
+              <Td>{doc.jumlah}</Td>
+              <Td>{doc.harga}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
     </Box>
   );
 }
