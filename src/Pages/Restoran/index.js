390253b370699603d564da51015bb0be022d4988
Tiberlium <lonew047@gmail.com>
2022-06-10 16:00:43 +0700

    perbaikan

---
+++ src/Pages/Restoran/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import {
+  Table,
+  Tr,
+  Td,
+  Thead,
+  Tbody,
+  TableCaption,
+  Th,
+  Button,
+  Text,
+  Box,
+  useToast,
+} from "@chakra-ui/react";
+import {
+  BsFillPencilFill,
+  BsFillTrashFill,
+  BsFillPlusCircleFill,
+} from "react-icons/bs";
+
+import { db, storages } from "../../Firebase";
+
+import { Link } from "react-router-dom";
+
+
+export default function Restoran() {
+  return <div>Restoran</div>;
+}
