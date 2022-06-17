4fe8cec694b9bacaf68dcc936a362c4578bac27f
Tiberlium <lonew047@gmail.com>
2022-06-17 23:53:12 +0700

    perbaikan

--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -5,45 +5,43 @@ import {
   Stat,
   StatLabel,
   StatNumber,
-  StatHelpText,
-  StatArrow,
   HStack,
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbLink,
 } from "@chakra-ui/react";
-import { BiMoney, BiUserCircle, BiTransferAlt } from "react-icons/bi";
 import { Link } from "react-router-dom";
+
 import { BsChevronRight } from "react-icons/bs";
 
-const Card = () => (
+import { BiUserCircle, BiTransferAlt, BiMoney } from "react-icons/bi";
+
+import { GiPayMoney } from "react-icons/gi";
+
+const Card = ({ pendapatan, pengguna, transaksi, total }) => (
   <HStack>
     <Stat borderWidth={1} p={5} w="2xs" borderRadius={10} boxShadow="base">
-      <BiMoney size={30} color="green" />
+      <BiMoney size={50} color="green" />
       <StatLabel>Pendapatan</StatLabel>
-      <StatNumber>Rp 10000</StatNumber>
-      <StatHelpText>
-        <StatArrow type="increase" />
-        10.2%
-      </StatHelpText>
+      <StatNumber>{pendapatan}</StatNumber>
     </Stat>
 
     <Stat borderWidth={1} p={5} w="2xs" borderRadius={10} boxShadow="base">
-      <BiUserCircle size={30} color="red" />
+      <BiUserCircle size={50} color="red" />
       <StatLabel>Pengguna</StatLabel>
-      <StatNumber>2</StatNumber>
-      <StatHelpText>
-        <StatArrow type="increase" />2
-      </StatHelpText>
+      <StatNumber>{pengguna}</StatNumber>
     </Stat>
 
     <Stat borderWidth={1} p={5} w="2xs" borderRadius={10} boxShadow="base">
-      <BiTransferAlt size={30} color="orange" />
+      <BiTransferAlt size={50} color="orange" />
       <StatLabel>Transaksi</StatLabel>
-      <StatNumber>7</StatNumber>
-      <StatHelpText>
-        <StatArrow type="increase" />5
-      </StatHelpText>
+      <StatNumber>{transaksi}</StatNumber>
+    </Stat>
+
+    <Stat borderWidth={1} p={5} w="2xs" borderRadius={10} boxShadow="base">
+      <GiPayMoney size={50} color="blue" />
+      <StatLabel>Total transaksi</StatLabel>
+      <StatNumber>{total}</StatNumber>
     </Stat>
   </HStack>
 );
@@ -62,7 +60,7 @@ export default function Dashboard() {
   return (
     <Box>
       <Navbread />
-      <Text fontSize="5xl" mb='12'>
+      <Text fontSize="5xl" mb="12">
         Dashboard anda
       </Text>
       <Card />
