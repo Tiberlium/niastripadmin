cafe6122b69c3bfeacedf75b3f60d278ddb56f58
Tiberlium <lonew047@gmail.com>
2022-06-18 21:19:42 +0700

    perbaikan

--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -12,62 +12,7 @@ import { BsChevronRight } from "react-icons/bs";
 
 import { db } from "../../Firebase";
 
-import { ResponsiveBar } from "@nivo/bar";
 import Card from "../../Component/Card";
-
-const data = [
-  {
-    day: "Monday",
-    degress: 59,
-  },
-  {
-    day: "Tuesday",
-    degress: 61,
-  },
-  {
-    day: "Wednesday",
-    degress: 55,
-  },
-  {
-    day: "Thursday",
-    degress: 78,
-  },
-  {
-    day: "Friday",
-    degress: 71,
-  },
-  {
-    day: "Saturday",
-    degress: 56,
-  },
-  {
-    day: "Sunday",
-    degress: 67,
-  },
-];
-
-const Bar = () => (
-  <Box h={"96"} w={"full"}>
-    <ResponsiveBar
-      data={data}
-      keys={["degress"]}
-      indexBy="day"
-      margin={{ top: 50, right: 150, bottom: 100, left: 60 }}
-      padding={0.7}
-      valueScale={{ type: "linear" }}
-      colors="#3182CE"
-      animate={true}
-      axisLeft={{
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: "degrees",
-        legendPosition: "middle",
-        legendOffset: -40,
-      }}
-    />
-  </Box>
-);
 
 const Navbread = () => (
   <Breadcrumb spacing="8px" separator={<BsChevronRight color="gray.500" />}>
@@ -149,6 +94,7 @@ export default function Dashboard() {
     return (pendapatan += Percentage(Number(doc["data"]["amount"]), 10));
   });
 
+
   return (
     <Box>
       <Navbread />
@@ -162,9 +108,8 @@ export default function Dashboard() {
         pendapatan={formatRupiah(pendapatan)}
       />
       <Text fontSize="2xl" color="black" mt={10}>
-        Aktivitas Pengguna
+        Aktivitas Transaksi
       </Text>
-      <Bar />
     </Box>
   );
 }
