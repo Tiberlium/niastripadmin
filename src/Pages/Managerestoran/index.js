e199941879ec1277e959afd6950634f5ee7daa0e
Tiberlium <lonew047@gmail.com>
2022-06-21 01:27:12 +0700

    perbaikan

--- src/Pages/Managerestoran/index.js
+++ src/Pages/Managerestoran/index.js
@@ -179,160 +179,156 @@ export default function Managerestoran() {
 
   useEffect(() => {
     get();
-    // eslint-disable-next-line react-hooks/exhaustive-deps 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const onChange = (imageList) => setimages(imageList);
 
   return (
-    <Center>
-      <Box width={"3xl"}>
-        <Breadcrumb
-          spacing="8px"
-          separator={<FiChevronRight color="gray.500" />}
-          mb={5}
-        >
-          <BreadcrumbItem>
-            <BreadcrumbLink as={Link} to="/Main">
-              Dashboard
-            </BreadcrumbLink>
-          </BreadcrumbItem>
+    <Box width={"3xl"} mr={32}>
+      <Breadcrumb
+        spacing="8px"
+        separator={<FiChevronRight color="gray.500" />}
+        mb={5}
+      >
+        <BreadcrumbItem>
+          <BreadcrumbLink as={Link} to="/Main">
+            Dashboard
+          </BreadcrumbLink>
+        </BreadcrumbItem>
 
-          <BreadcrumbItem>
-            <BreadcrumbLink as={Link} to="/Main/Manage">
-              Kelola
-            </BreadcrumbLink>
-          </BreadcrumbItem>
+        <BreadcrumbItem>
+          <BreadcrumbLink as={Link} to="/Main/Manage">
+            Kelola
+          </BreadcrumbLink>
+        </BreadcrumbItem>
 
-          <BreadcrumbItem>
-            <BreadcrumbLink as={Link} to="/Main/Restoran">
-              Tempat makan
-            </BreadcrumbLink>
-          </BreadcrumbItem>
+        <BreadcrumbItem>
+          <BreadcrumbLink as={Link} to="/Main/Restoran">
+            Tempat makan
+          </BreadcrumbLink>
+        </BreadcrumbItem>
 
-          <BreadcrumbItem isCurrentPage>
-            <BreadcrumbLink as={Link} to="#">
-              Form Tempat makan
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-        </Breadcrumb>
-        <Box>
-          <Text fontSize={"4xl"}>Form Tempat Makan</Text>
-          <Wrap>
-            <WrapItem>
-              <Box>
-                <FormControl required width={"3xl"} mt={10}>
-                  <FormLabel htmlFor="Nama restoran">Nama Restoran</FormLabel>
-                  <Input
-                    placeholder="Nama Restoran"
-                    variant={"filled"}
-                    type={"text"}
-                    defaultValue={nama || ""}
-                    onChange={(e) => setnama(e.target.value)}
-                  />
-                  <FormHelperText>masukkan nama tempat restoran</FormHelperText>
-                </FormControl>
-                <FormControl required mt={5}>
-                  <FormLabel htmlFor="Kontak">Kontak</FormLabel>
-                  <Input
-                    placeholder="masukkan Kontak"
-                    variant={"filled"}
-                    defaultValue={kontak || ""}
-                    onChange={(e) => setkontak(e.target.value)}
-                  />
-                  <FormHelperText>masukkan kontak restoran</FormHelperText>
-                </FormControl>
-                <FormControl required mt={5}>
-                  <FormLabel htmlFor="Lokasi">Lokasi</FormLabel>
-                  <Input
-                    placeholder="Lokasi"
-                    variant={"filled"}
-                    type={"text"}
-                    defaultValue={alamat || ""}
-                    onChange={(e) => setalamat(e.target.value)}
-                  />
-                  <FormHelperText>
-                    masukkan nama jalan dimana restoran berada
-                  </FormHelperText>
-                </FormControl>
-                <FormControl mt={5}>
-                  <FormLabel htmlFor="Jam Operasional">
-                    Jam operasional
-                  </FormLabel>
-                  <Input
-                    placeholder="Jam operasional"
-                    variant={"filled"}
-                    type={"text"}
-                    defaultValue={operasional || ""}
-                    onChange={(e) => setoperasional(e.target.value)}
-                  />
-                  <FormHelperText>
-                    Masukkan jam operasional restoran
-                  </FormHelperText>
-                </FormControl>
-              </Box>
-            </WrapItem>
-          </Wrap>
-        </Box>
-        <FormLabel mt={5}>Lokasi Wisata</FormLabel>
-        <Map
-          provider={osm}
-          height={400}
-          width={765}
-          dprs={[1, 2]}
-          defaultCenter={[1.1603381323455186, 97.52212877347822]}
-          center={[latitude, longitude]}
-          defaultZoom={12}
-          onClick={(e) => {
-            setlatitude(e.latLng[0]);
-            setlongitude(e.latLng[1]);
-          }}
-        >
-          <Marker color="red" width={40} />
-          <ZoomControl />
-        </Map>
-        <Box>
-          <FormLabel mt={5}>Gambar</FormLabel>
-          <ImageUploading multiple value={images} onChange={onChange}>
-            {({
-              imageList,
-              onImageUpload,
-              onImageRemove,
-              isDragging,
-              dragProps,
-            }) => (
-              <Box>
-                <Button
-                  width={"full"}
-                  leftIcon={<FiUpload />}
-                  colorScheme={isDragging ? "red" : "teal"}
-                  onClick={onImageUpload}
-                  {...dragProps}
-                >
-                  Click or Drop here
-                </Button>
-                {imageList.map((image, index) => (
-                  <Imagescard
-                    key={index}
-                    label={image.file.name}
-                    onClick={() => onImageRemove(index)}
-                  />
-                ))}
-              </Box>
-            )}
-          </ImageUploading>
-        </Box>
-        <Button
-          colorScheme={"blue"}
-          marginTop={"6"}
-          width={"full"}
-          alignSelf={"center"}
-          onClick={handleUpload}
-          isLoading={loading}
-        >
-          {id ? "Update" : "Submit"}
-        </Button>
+        <BreadcrumbItem isCurrentPage>
+          <BreadcrumbLink as={Link} to="#">
+            Form Tempat makan
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+      </Breadcrumb>
+      <Box>
+        <Text fontSize={"4xl"}>Form Tempat Makan</Text>
+        <Wrap>
+          <WrapItem>
+            <Box>
+              <FormControl required width={"3xl"} mt={10}>
+                <FormLabel htmlFor="Nama restoran">Nama Restoran</FormLabel>
+                <Input
+                  placeholder="Nama Restoran"
+                  variant={"filled"}
+                  type={"text"}
+                  defaultValue={nama || ""}
+                  onChange={(e) => setnama(e.target.value)}
+                />
+                <FormHelperText>masukkan nama tempat restoran</FormHelperText>
+              </FormControl>
+              <FormControl required mt={5}>
+                <FormLabel htmlFor="Kontak">Kontak</FormLabel>
+                <Input
+                  placeholder="masukkan Kontak"
+                  variant={"filled"}
+                  defaultValue={kontak || ""}
+                  onChange={(e) => setkontak(e.target.value)}
+                />
+                <FormHelperText>masukkan kontak restoran</FormHelperText>
+              </FormControl>
+              <FormControl required mt={5}>
+                <FormLabel htmlFor="Lokasi">Lokasi</FormLabel>
+                <Input
+                  placeholder="Lokasi"
+                  variant={"filled"}
+                  type={"text"}
+                  defaultValue={alamat || ""}
+                  onChange={(e) => setalamat(e.target.value)}
+                />
+                <FormHelperText>
+                  masukkan nama jalan dimana restoran berada
+                </FormHelperText>
+              </FormControl>
+              <FormControl mt={5}>
+                <FormLabel htmlFor="Jam Operasional">Jam operasional</FormLabel>
+                <Input
+                  placeholder="Jam operasional"
+                  variant={"filled"}
+                  type={"text"}
+                  defaultValue={operasional || ""}
+                  onChange={(e) => setoperasional(e.target.value)}
+                />
+                <FormHelperText>
+                  Masukkan jam operasional restoran
+                </FormHelperText>
+              </FormControl>
+            </Box>
+          </WrapItem>
+        </Wrap>
       </Box>
-    </Center>
+      <FormLabel mt={5}>Lokasi Wisata</FormLabel>
+      <Map
+        provider={osm}
+        height={400}
+        width={765}
+        dprs={[1, 2]}
+        defaultCenter={[1.1603381323455186, 97.52212877347822]}
+        center={[latitude, longitude]}
+        defaultZoom={12}
+        onClick={(e) => {
+          setlatitude(e.latLng[0]);
+          setlongitude(e.latLng[1]);
+        }}
+      >
+        <Marker color="red" width={40} />
+        <ZoomControl />
+      </Map>
+      <Box>
+        <FormLabel mt={5}>Gambar</FormLabel>
+        <ImageUploading multiple value={images} onChange={onChange}>
+          {({
+            imageList,
+            onImageUpload,
+            onImageRemove,
+            isDragging,
+            dragProps,
+          }) => (
+            <Box>
+              <Button
+                width={"full"}
+                leftIcon={<FiUpload />}
+                colorScheme={isDragging ? "red" : "teal"}
+                onClick={onImageUpload}
+                {...dragProps}
+              >
+                Click or Drop here
+              </Button>
+              {imageList.map((image, index) => (
+                <Imagescard
+                  key={index}
+                  label={image.file.name}
+                  onClick={() => onImageRemove(index)}
+                />
+              ))}
+            </Box>
+          )}
+        </ImageUploading>
+      </Box>
+      <Button
+        colorScheme={"blue"}
+        marginTop={"6"}
+        width={"full"}
+        alignSelf={"center"}
+        onClick={handleUpload}
+        isLoading={loading}
+      >
+        {id ? "Update" : "Submit"}
+      </Button>
+    </Box>
   );
 }
